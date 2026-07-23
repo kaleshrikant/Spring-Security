de83f722466885cb329188ecb4421d6cc8dc6a25
Shrikant Kale <kaleshrikant@live.com>
2026-07-23 16:47:59 +0530

    initial commit

---
+++ section-1/spring-sec-sec1/src/main/java/com/kaleshrikant/SpringSecSec1Application.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecSec1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecSec1Application.class, args);
+	}
+
+}
