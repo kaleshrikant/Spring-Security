06a5c8dd06a55b368e1bc8f554674f90d1a20ee5
Shrikant Kale <kaleshrikant@live.com>
2026-07-24 20:26:42 +0530

    InMemoryUserDetailsManager without password encoder

--- section-3/spring-sec-sec3/src/main/java/com/kaleshrikant/config/ProjectSecurityConfig.java
+++ section-3/spring-sec-sec3/src/main/java/com/kaleshrikant/config/ProjectSecurityConfig.java
@@ -3,6 +3,10 @@ package com.kaleshrikant.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -23,5 +27,12 @@ public class ProjectSecurityConfig {
 		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
 		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+	    UserDetails user =  User.withUsername("user").password("{noop}user").authorities("read").build();
+	    UserDetails admin =  User.withUsername("admin").password("{noop}admin").authorities("admin").build();
+		return new InMemoryUserDetailsManager(user,admin);
 	}
 }
