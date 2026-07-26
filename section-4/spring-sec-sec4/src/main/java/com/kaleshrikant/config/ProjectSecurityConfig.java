49b01b3cff84ed51f20ebbd0f409a0fa41d805eb
Shrikant Kale <kaleshrikant@live.com>
2026-07-26 16:15:15 +0530

    Added JdbcUserDetailsManager

--- section-4/spring-sec-sec4/src/main/java/com/kaleshrikant/config/ProjectSecurityConfig.java
+++ section-4/spring-sec-sec4/src/main/java/com/kaleshrikant/config/ProjectSecurityConfig.java
@@ -4,20 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * @author Shrikant Kale
- * @Date 24 Jul 2026
+ * @Date 26 Jul 2026
  */
 
 @Configuration
@@ -34,10 +34,8 @@ public class ProjectSecurityConfig {
 	}
 
 	@Bean
-	public UserDetailsService userDetailsService() {
-	    UserDetails user =  User.withUsername("user").password("{noop}user").authorities("read").build();
-	    UserDetails admin =  User.withUsername("admin").password("{bcrypt}$2a$12$FJ32ahdS/KMBDcYh19hGSuwo5zJW00oaqx9L7ygICnwg0M1VBVujO").authorities("admin").build();
-		return new InMemoryUserDetailsManager(user,admin);
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
 	}
 
 	// https://bcrypt-generator.com/
