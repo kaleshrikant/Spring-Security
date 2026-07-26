a10a3744516810b6cfa125188a78251dd2e6af92
Shrikant Kale <kaleshrikant@live.com>
2026-07-26 16:31:28 +0530

    custom table created for JdbcUserDetailsManager

--- section-4/spring-sec-sec4/src/main/resources/sql/scripts.sql
+++ section-4/spring-sec-sec4/src/main/resources/sql/scripts.sql
@@ -23,3 +23,18 @@ VALUES ('user', 'read');
 
 INSERT INTO authorities (username, authority)
 VALUES ('admin', 'admin');
+
+-- ------------------------------------- CUSTOM TABLE --------------------------------------------- --
+CREATE TABLE customer (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    pwd VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
+
+-- Insert customers
+INSERT INTO customer (email, pwd, role)
+VALUES ('user@example.com', '{noop}user', 'read');
+
+INSERT INTO customer (email, pwd, role)
+VALUES ('admin@example.com', '{bcrypt}$2a$12$FJ32ahdS/KMBDcYh19hGSuwo5zJW00oaqx9L7ygICnwg0M1VBVujO', 'admin');
